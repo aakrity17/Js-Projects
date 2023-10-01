9405d20fa25dbae73c157aedf63066530a01decb
Aakrity Chapagai <aakritycpg@gmail.com>
2023-10-01 18:54:15 +0545

    playing with dom

--- 15.HtmlDOm/index.js
+++ 15.HtmlDOm/index.js
@@ -1,12 +1,18 @@
 const header = document.getElementById("header");
 const btn = document.getElementById("btn");
 const name = document.getElementById("name");
+const body = document.getElementById("body");
 
 btn.addEventListener("click", (e) => {
     e.preventDefault();
-    header.style.color ="red";
-    header.innerHTML = "I am " + name.value;
-    console.log(name.value);
+
+    // let color = name.value;
+    body.style.backgroundColor = name.value;
+    name.value = "";
+    // console.log(name.value);
+    header.style.color = "white";
+    // header.innerHTML = "I am " + name.value;
+    console.log("hello");
+    localStorage.setItem(key.value, data.value);
 });
 
-console.log(header.innerHTML);
